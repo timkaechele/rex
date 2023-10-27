59420ee879f005bb84256ae36a5b5325af8f738b
Tim Kächele <mail@timkaechele.me>
2023-10-27 14:56:05 +0200

    Implement order model

---
+++ lib/rex/order.rb
@@ -0,0 +1,29 @@
+module Rex
+  class Order
+    attr_accessor(
+      :next_order,
+      :remaining_amount
+    )
+
+    attr_reader(
+      :id,
+      :is_buy,
+      :price,
+      :user_id,
+      :amount
+    )
+
+    def initialize(attrs = {})
+      @id = attrs[:id]
+
+      @user_id = attrs[:user_id]
+      @price = attrs[:price]
+      @is_buy = attrs[:is_buy]
+      @amount = attrs[:amount]
+      @remaining_amount = attrs[:amount]
+
+      @next_order = nil
+    end
+
+  end
+end
