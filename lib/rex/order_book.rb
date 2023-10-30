0efb6b0e88bdc93dc1f0bc52e8aed63dcf152f80
Tim Kächele <mail@timkaechele.me>
2023-10-30 20:43:26 +0100

    Remove limit volume tracking from order book
    It is not super relevant for the current matching engine,
    we are going to solve this in the server implementation

--- lib/rex/order_book.rb
+++ lib/rex/order_book.rb
@@ -2,10 +2,6 @@ require "rbtree"
 
 module Rex
   class OrderBook
-    attr_reader(
-      :buy_limit_volumes,
-      :sell_limit_volumes
-    )
     def initialize(matcher: Matcher.new)
       @matcher = matcher
       @sell_side = RBTree.new
@@ -13,19 +9,15 @@ module Rex
       @order_ids = {} # order_id => order
       @current_trade_id = 0
       @current_order_id = 0
-      @buy_limit_volumes = {} # price => volume
-      @sell_limit_volumes = {} # price => volume
     end
 
     def add_order(order)
       side = side_for_order(order)
-      limit_volumes = limit_volume_for_order_side(order)
       order.id = next_order_id
 
       side[order.price] ||= Limit.new(order.price)
       side[order.price].add_order(order)
-      limit_volumes[order.price] ||= 0
-      limit_volumes[order.price] += order.remaining_quantity
+
       order_ids[order.id] = order
     end
 
@@ -46,9 +38,6 @@ module Rex
         side.delete(limit.price)
       end
 
-      limit_volumes = limit_volume_for_order_side(order)
-      limit_volumes[order.price] -= order.remaining_quantity
-
       order_ids.delete(order.id)
     end
 
@@ -57,9 +46,6 @@ module Rex
     def process_trade(trade)
       trade.buy_order.remaining_quantity -= trade.quantity
       trade.sell_order.remaining_quantity -= trade.quantity
-
-      buy_limit_volumes[trade.buy_order.price] -= trade.quantity
-      sell_limit_volumes[trade.sell_order.price] -= trade.quantity
 
       remove_order(trade.buy_order.id) if trade.buy_order.filled?
       remove_order(trade.sell_order.id) if trade.sell_order.filled?
@@ -95,14 +81,6 @@ module Rex
 
     def next_order_id
       @current_order_id += 1
-    end
-
-    def limit_volume_for_order_side(order)
-      if order.is_buy
-        @buy_limit_volumes
-      else
-        @sell_limit_volumes
-      end
     end
 
     def side_for_order(order)
