b7df45794c8cb63bba31a79a9f4cbe4201fbb4c5
Tim Kächele <mail@timkaechele.me>
2023-11-08 22:44:47 +0100

    Refactor how we require the book commponents

--- lib/rex.rb
+++ lib/rex.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "rex/version"
-require_relative "rex/book/limit"
-require_relative "rex/book/order"
-require_relative "rex/book/trade"
-require_relative "rex/book/limit_order_book"
-require_relative "rex/book/matcher"
+require_relative "rex/book"
 
 module Rex
   class Error < StandardError; end
---
+++ lib/rex/book.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "book/limit"
+require_relative "book/trade"
+require_relative "book/order"
+require_relative "book/limit_order_book"
+require_relative "book/matcher"
+
+module Rex
+  module Book
+  end
+end
